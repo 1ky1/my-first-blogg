1a9e7de29d882ca9994fcefb8e7bd647ebd21f3c
kyvinay <kyvinay@gmail.com>
2019-07-15 23:01:34 +0530

    Changed from django.shortcuts a litll

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
-from django.shortcuts import redirect
+#from django.shortcuts import redirect
 
 
 
